76032aa3c2c7aa6432e7ad962ab2fd6cb82f9fb2
krushna <ingalekrushna3003@gmail.com>
2025-01-22 03:04:18 +0530

    Product page added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { ShopContext } from '../context/ShopContext';
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
-      const {search,setSearch,showSearch,setShowSearch} = useContext(ShopContext);
+      const {getCartCount,setShowSearch} = useContext(ShopContext);
   
 
   return (
@@ -46,7 +46,7 @@ const Navbar = () => {
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="cart icon" />
           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
-            {/* Dynamic cart item count */}
+            {/* Dynamic cart item count */}{getCartCount()}
           </p>
         </Link>
 
--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 import {products} from '../assets/assets'
+import { toast } from "react-toastify";
 export const ShopContext = createContext();
 
 export const ShopContextProvider = (props)=>{
@@ -8,13 +9,58 @@ export const ShopContextProvider = (props)=>{
     const delivery_fee = 10;
     const [search, setSearch] = useState(''); 
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({});
 
+    const addToCart = async(ItemId, size)=>{
+
+        if(!size){
+            toast.error('Select Product size')
+            return;
+        }
+        let cartData = structuredClone(cartItems)
+
+
+        if(cartData[ItemId]){
+            if(cartData[ItemId][size]){
+                cartData[ItemId][size] += 1;
+
+            }else{
+                cartData[ItemId][size] = 1;
+            }
+        }else{
+            cartData[ItemId] = {};
+            cartData[ItemId][size] = 1;
+        }
+        setCartItems(cartData);
+    }
+
+    const getCartCount = ()=>{
+        let totalCount = 0;
+        for(const items in cartItems){
+            for(const item in cartItems[items] ){
+                try {
+                    if(cartItems[items][item]> 0){
+                        totalCount += cartItems[items][item]
+                    }
+        
+                } catch (error) {
+                    console.error(error.message);
+                    
+                }
+            }
+        }
+        return totalCount
+    }
+
+
+   
 
     const value = {
         products,
         currency,
         delivery_fee,
-        search,setSearch,showSearch,setShowSearch
+        search,setSearch,showSearch,setShowSearch,
+        cartItems,addToCart, getCartCount
     }
 
     return (
